2bc4c7f816e5868d53ff440556897ae6f0432f5e
idoyanay1230 <ido.yanay@mail.huji.ac.il>
2025-04-27 00:32:08 +0300

    --- 2025-04-27 00:32 --- sutarday commit

--- uthreads.cpp
+++ uthreads.cpp
@@ -8,9 +8,11 @@
 #include <iostream>
 #include <cstdlib>     // for exit()
 #include <list>        // for the list of READY threads
+#include <queue>       // for std::priority_queue
 #include <csetjmp>     // for sigjmp_buf
 #include <setjmp.h>
 #include <csignal>     // for sigemptyset
+#include <sys/time.h>  // for itimerval
 
 
  
@@ -30,21 +32,57 @@ struct Thread {
 
 std::list<Thread*> ready_threads;
 std::list<Thread*> blocked_threads;
+
+// this following structure will contatain the tid that have been used, for givin there tid to new threads.
+std::priority_queue<int, std::vector<int>, std::greater<int>> unused_tid;
+
 int threads_counter;
 Thread* running_thread;
+int quantum_per_thread;
 
 
  // ------------------------------------------------------------------------- //
  
 void print_error(const std::string& msg, PrintType type) {
-     if (type == PrintType::SYSTEM_ERR) {
-         std::cerr << "system error: " << msg << std::endl;
-         std::exit(1); // Immediately exit with failure
-     } else if (type == PrintType::THREAD_LIB_ERR) {
-         std::cerr << "thread library error: " << msg << std::endl;
-         // Do NOT exit — the calling function should handle the return value
-     }
+    if (type == PrintType::SYSTEM_ERR) {
+        std::cerr << "system error: " << msg << std::endl;
+        std::exit(1); // Immediately exit with failure
+    } else if (type == PrintType::THREAD_LIB_ERR) {
+        std::cerr << "thread library error: " << msg << std::endl;
+        // Do NOT exit — the calling function should handle the return value
+    }
  }
+
+
+// TODO: every time that usign this function, there is a negligible time that the thread is not running, 
+// but the timer is working due to the time that take the return to go. 
+// maybe copy-paste the code to every use of the function instead of general function.
+int set_thread_timer(void){
+    struct itimerval it_value;
+    // the timer will not reset after the first call when initializing the it_interval like this
+    it_value.it_interval.tv_sec = 0;
+    it_value.it_interval.tv_usec = 0;
+
+    it_value.it_value.tv_sec = 0;
+    it_value.it_value.tv_usec = quantum_per_thread;
+    return setitimer(ITIMER_VIRTUAL, &it_value, NULL);
+}
+
+void end_of_quantum(int sig)
+{
+    if (threads_counter == 1){
+        return; // nothing to do
+    }
+    
+    running_thread = ready_threads.front(); // setting the thread to run
+    ready_threads.pop_front(); // removing the thread from the list
+    ready_threads.push_back(running_thread); // pushing the thread to the end of the list
+    if(set_thread_timer != 0){
+        print_error("setitimer failed", PrintType::SYSTEM_ERR); // this call will end the run with exit(1)
+    }
+    siglongjmp(running_thread->env, 1); // jumping to the thread's context
+
+}
 
 address_t translate_address(address_t addr)
 {
@@ -68,28 +106,30 @@ void setup_thread(char *stack, thread_entry_point entry_point, sigjmp_buf env)
  
 
 int uthread_init(int quantum_usecs) {
-     if (quantum_usecs <= 0) { 
-         print_error("uthread_init: quantum_usecs must be positive", PrintType::THREAD_LIB_ERR);
-         return -1;
-     }
-     threads_counter = 1;
-     running_thread = new Thread{0, ThreadState::RUNNING, {}, 0, {}}; // initializing main thread
-     sigsetjmp(running_thread->env, 1); // Save current CPU context
-     return 0;
+    if (quantum_usecs <= 0) { 
+        print_error("uthread_init: quantum_usecs must be positive", PrintType::THREAD_LIB_ERR);
+        return -1;
+    }
+
+    quantum_per_thread = quantum_usecs;
+    threads_counter = 1;
+    running_thread = new Thread{0, ThreadState::RUNNING, {}, 0, {}}; // initializing main thread
+    sigsetjmp(running_thread->env, 1); // Save current CPU context
+
+    struct sigaction sa = {0};
+    sa.sa_handler = &end_of_quantum;
+    if (sigaction(SIGVTALRM, &sa, NULL) < 0)
+    {
+        print_error("uthread_init: sigaction failed", PrintType::SYSTEM_ERR); // TODO: make sure this is the type of error
+    }
+
+    if(set_thread_timer != 0){
+        print_error("uthread_init: setitimer failed", PrintType::SYSTEM_ERR); // this call will end the run with exit(1)
+    }
+    return 0;
  }
 
- /**
- * @brief Creates a new thread, whose entry point is the function entry_point with the signature
- * void entry_point(void).
- *
- * The thread is added to the end of the READY threads list.
- * The uthread_spawn function should fail if it would cause the number of concurrent threads to exceed the
- * limit (MAX_THREAD_NUM).
- * Each thread should be allocated with a stack of size STACK_SIZE bytes.
- * It is an error to call this function with a null entry_point.
- *
- * @return On success, return the ID of the created thread. On failure, return -1.
-*/
+
 int uthread_spawn(thread_entry_point entry_point){
     
     if(threads_counter == MAX_THREAD_NUM) { // check if the number of threads is already at the maximum
@@ -106,4 +146,16 @@ int uthread_spawn(thread_entry_point entry_point){
     ready_threads.push_back(new_thread); // add the new thread to the ready threads list
     return new_thread->tid;
 }
+
+/**
+ * @brief Terminates the thread with ID tid and deletes it from all relevant control structures.
+ *
+ * All the resources allocated by the library for this thread should be released. If no thread with ID tid exists it
+ * is considered an error. Terminating the main thread (tid == 0) will result in the termination of the entire
+ * process using exit(0) (after releasing the assigned library memory).
+ *
+ * @return The function returns 0 if the thread was successfully terminated and -1 otherwise. If a thread terminates
+ * itself or the main thread is terminated, the function does not return.
+*/
+int uthread_terminate(int tid);
  
