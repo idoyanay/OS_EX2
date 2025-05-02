8e7ba2d587929b448beeff88926eebf3758f0522
idoyanay1230 <ido.yanay@mail.huji.ac.il>
2025-05-02 14:31:59 +0300

    --- 2025-05-02 14:31 ---

--- test0_sanity.cpp
+++ test0_sanity.cpp
@@ -59,7 +59,7 @@ int main(void)
 				std::cout << "m" << tid << " Quanta:" <<  i << std::endl;
 				if (i == 3)
 				{
-					// std::cout << "m spawns f at (1) " << uthread_spawn(f) << std::endl;
+					std::cout << "m spawns f at (1) " << uthread_spawn(f) << std::endl;
 					// std::cout << "m spawns g at (2) " << uthread_spawn(g) << std::endl;
 				}
 				if (i == 100)
