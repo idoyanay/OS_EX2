298e2e80a228851a8e23d6e215ea2fd7e86d3671
idoyanay1230 <ido.yanay@mail.huji.ac.il>
2025-05-03 18:19:32 +0300

    --- 2025-05-03 18:19 ---

--- uthreads_test.cpp
+++ uthreads_test.cpp
@@ -360,6 +360,8 @@ void test_thread_functions_in_threads() {
     thread_ids[spawner_tid] = 1;
     
     // Wait for threads to run
+    int current_quantom_counter = uthread_get_total_quantums();
+    while(uthread_get_total_quantums() < current_quantom_counter + 5);
     for (volatile int i = 0; i < 2000000; i++) {}
     
     // Check that global_counter has been incremented at least twice
